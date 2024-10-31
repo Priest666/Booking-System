17e76fb318e0c29aec53219bda4bb7d1899bf148
Thimmy Severin <thimmy.severin@learnet.se>
2024-10-31 13:23:09 +0100

    Lagt till prop på premisis och metod displayinfo

--- Premises.cs
+++ Premises.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Booking_System
 {
-    internal class Premises
+    abstract class Premises
     {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
 
+        public Premises(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public abstract void DisplayInfo();
     }
 }
