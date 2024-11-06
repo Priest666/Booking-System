87edd458a55218cda9d5f6cb680f07ca4151d3f5
Jonatan Präst <jonatan.prast@learnet.se>
2024-11-06 14:18:41 +0100

    Radera oanvändkod

--- Booking.cs
+++ Booking.cs
@@ -72,7 +72,6 @@ namespace Booking_System
                     }
 
                     Console.WriteLine("Choose capacity: ");
-                    bool isValid = false;
                     int ClassRoomCap;
                     while (!int.TryParse(Console.ReadLine(), out ClassRoomCap))
                     {
